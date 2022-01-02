fb06669e449ca419015bd78a665973d161f84a4b
SatoJura <jnnsk0snb@gmail.com>
2022-01-02 21:39:19 +0900

    [Add]Chapter05 プリミティブ型の「double」

--- src/Chapter05.java
+++ src/Chapter05.java
@@ -4,6 +4,10 @@ public class Chapter05 {
 		//プリミティブ型の「long」
 		long long1 = 9223372036854775807L;
 		System.out.println(long1);
+		
+		//プリミティブ型の「double」
+		double double1 = 1234567.89;
+		System.out.println(double1);
 	}
 
 }
