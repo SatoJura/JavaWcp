5ef90cb55dcba3246801b9ccac7902c7fd006cef
SatoJura <jnnsk0snb@gmail.com>
2022-01-02 20:50:35 +0900

    [Add]Chapter04

---
+++ src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main (String[] args) {
+		//変数の宣言（名前）
+		String str1 = "Sato";
+		System.out.println(str1);
+		
+		//同じ変数に再代入（好きな言葉）
+		str1 = "ほかほか";
+		System.out.println(str1);
+		
+		//定数の宣言（ラッキーナンバー）
+		final int NUMBER = 2; 
+		System.out.println(NUMBER);
+	}
+}
