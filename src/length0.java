901d3858d72c8a2d40e4f1eccc87b084a12c2e58
SatoJura <jnnsk0snb@gmail.com>
2022-01-04 21:44:38 +0900

    [Add]length0

---
+++ src/length0.java
@@ -0,0 +1,13 @@
+
+public class length0 {
+	public static void main (String[] args) {
+		//length:配列の長さを取得する
+		int[] array = {0, 1, 2, 3};
+		System.out.println (array.length); //4
+		
+		//length():文字列の長さを取得する
+		String str = "lengthの長さ!";
+		System.out.println (str.length()); //10
+	}
+
+}
