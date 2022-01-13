989a29967d276764e5b17c688374b6601b0ddd92
SatoJura <jnnsk0snb@gmail.com>
2022-01-13 23:44:49 +0900

    [Add]FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,29 @@
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+    /**
+     * 1から順番に数を表示
+     * その数が3で割り切れるなら"Fizz"、5で割り切れるなら"Buzz、
+     * 両方で割り切れるなら"FizzBuzz" と表示
+     */
+		int i = 0;
+		int loopCount = 100;
+		int numFizz = 3;
+		int numBuzz = 5;
+		
+		String strFizz = "Fizz";
+		String strBuzz = "Buzz";
+		
+		for (i = 1; i <= loopCount; i++) {
+			if (i % (numFizz * numBuzz) == 0) {
+				System.out.println(strFizz + strBuzz);
+			} else if (i % numFizz == 0) {
+				System.out.println(strFizz);
+			} else if (i % numBuzz == 0) {
+				System.out.println(strBuzz);
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
+}
