5f65ee5b3155a37bf8134a48d0c71ac193577c62
SatoJura <jnnsk0snb@gmail.com>
2022-01-13 23:13:07 +0900

    [Add]Chapter08 拡張for文

--- src/Chapter08.java
+++ src/Chapter08.java
@@ -2,9 +2,17 @@
 public class Chapter08 {
 	public static void main(String[] args) {
 		// for文
-		int[] array = {2, 4, 6, 8};
+		int[] array = {1, 4, 6, 11};
 		for (int i = 0; i < array.length; i++) {
 			System.out.println(array[i]);
+		}
+		
+		// 拡張for文
+		for (int arr : array) {
+			if(arr % 2 == 0) {
+				continue;
+			}
+			System.out.println(arr);
 		}
 	}
 }
