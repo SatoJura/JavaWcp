9ffd27ed448d795215251e4abe5f8a65d27338ae
SatoJura <jnnsk0snb@gmail.com>
2022-01-16 22:44:42 +0900

    [Add]ForFor0 ②

--- src/ForFor0.java
+++ src/ForFor0.java
@@ -1,7 +1,7 @@
 
 public class ForFor0 {
 	public static void main(String[] args) {
-		// for構文の中にfor構文
+		// for構文の中にfor構文①
 		for (int i = 1; i <= 3; i++) {
 			for (int j = 3; j >= 1; j--) {
 				if (i != j) {
@@ -10,6 +10,7 @@ public class ForFor0 {
 			}
 		}
 		
+		// for構文の中にfor構文②
 		for (int i = 1; i <= 3; i++) {
 			for (int j = 1; j <= 3; j++) {
 				System.out.println(i + " 年 " + j + " 組");
