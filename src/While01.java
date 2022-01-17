be0e211b9415d995b126191025ef3c674b2476d8
SatoJura <jnnsk0snb@gmail.com>
2022-01-17 12:54:46 +0900

    [Add] 10→1まで出力

--- src/While01.java
+++ src/While01.java
@@ -1,10 +1,18 @@
 
 public class While01 {
 	public static void main(String[] args) {
+		// 2の倍数を繰り返し出力
 		int number = 1;
 		while (number < 50) {
 			number *= 2;
 			System.out.println("While01 = " + number);
+		}
+		
+		// numberが0より大きい場合に繰り返す
+		int i = 10;
+		while (i > 0) {
+			System.out.println(i);
+			i--;
 		}
 	}
 
