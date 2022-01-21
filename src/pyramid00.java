12c6bb885bba75084e4cea9769091c9d55a9faaa
SatoJura <jnnsk0snb@gmail.com>
2022-01-21 17:29:24 +0900

    [Add]pyramid00

---
+++ src/pyramid00.java
@@ -0,0 +1,18 @@
+
+public class pyramid00 {
+	public static void main(String[] args) {
+		// 段数
+		for (int i=0; i<5; i++) {
+			// 左側の空白
+			for (int j=0; j<5-(i+1); j++) {
+				System.out.print(" ");
+			}
+			// 空白の右隣に✳
+			for (int k=0; k<(i+1)*2-1; k++) {
+				System.out.print("*");
+			}
+			// 改行
+			System.out.print("\n");
+		}
+	}
+}
