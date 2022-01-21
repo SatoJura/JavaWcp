f99b700bed22f55087897ca1eb2e06b36d77e54c
SatoJura <jnnsk0snb@gmail.com>
2022-01-21 16:36:27 +0900

    [Add]pyramid0 ③3段逆ピラミッド

--- src/pyramid0.java
+++ src/pyramid0.java
@@ -8,7 +8,8 @@ public class pyramid0 {
 			for (int j=0; j<i; j++) {
 				System.out.print(" ");
 			}
-			// 空白の隣に✳(k) 奇数になるように
+			/* 空白の隣にアスタリスク(k)、x2-1で奇数にする
+			 (段数-i)x2-1 */
 			for (int k=0; k<(5-i)*2-1; k++) {
 				System.out.print("*");
 			}
@@ -21,6 +22,17 @@ public class pyramid0 {
 				System.out.print(" ");
 			}
 			for (int ast=0; ast<(5-a)*2-1; ast++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+		
+		// ③3段逆ピラミッド
+		for (int c=0; c<3; c++) {
+			for (int sp=0; sp<c; sp++) {
+				System.out.print(" ");
+			}
+			for (int as=0; as<(3-c)*2-1; as++) {
 				System.out.print("*");
 			}
 			System.out.print("\n");
