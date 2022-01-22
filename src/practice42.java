da1d03413cd46e4f4c518ca2d31c5dacc19e8fee
SatoJura <jnnsk0snb@gmail.com>
2022-01-23 00:02:59 +0900

    [Add]practice42

---
+++ src/practice42.java
@@ -0,0 +1,13 @@
+
+public class practice42 {
+	public static void main(String[] args) {
+		int i=0;
+		int loopcount=27;
+		for (i=1;i<loopcount;i++) {
+			if (i % 3 == 0) {
+				System.out.println("3の倍数は "+i);
+			}
+		}
+	}
+
+}
