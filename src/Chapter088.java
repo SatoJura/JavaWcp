2a58745a462ae38e4b7be54631b5934d64e3d43c
SatoJura <jnnsk0snb@gmail.com>
2022-02-12 14:49:58 +0900

    [Add]Chapter088 ①

---
+++ src/Chapter088.java
@@ -0,0 +1,13 @@
+
+public class Chapter088 {
+	public static void main (String[] args) {
+		// 1.「while」文
+		// 1以上5未満の数の2乗を出力
+		int i = 1;
+		while(i < 5) {
+			System.out.println("1." + i * i);
+			i++;
+		}
+	}
+
+}
