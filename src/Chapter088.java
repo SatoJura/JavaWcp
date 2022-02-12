f851129bc24cfa163e94da05c8174ebf93d16866
SatoJura <jnnsk0snb@gmail.com>
2022-02-12 14:52:22 +0900

    [Add]Chapter088 ②

--- src/Chapter088.java
+++ src/Chapter088.java
@@ -8,6 +8,16 @@ public class Chapter088 {
 			System.out.println("1." + i * i);
 			i++;
 		}
+		
+		/* 2. 通常「for」文
+		　・要素数が4の「int」配列を初期化
+		　・配列のサイズ分ループして、要素を出力
+		　※ 配列のサイズは「変数名.length」で取得が可能
+		*/
+		int[] array= {1,2,3,4};
+		for (int j = 0; j < array.length; j++) {
+			System.out.println("2." + array[j]);
+		}
 	}
 
 }
